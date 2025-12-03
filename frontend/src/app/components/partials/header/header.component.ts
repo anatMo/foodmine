8cd6b675ec4a75d85dab4783017607fd4825e72b
anatmo <1.9.9.4anat@gmail.com>
2025-12-03 14:51:29 +0200

    lconnect login to header fllow

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/User';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +11,15 @@ import { CartService } from 'src/app/services/cart.service';
 export class HeaderComponent implements OnInit {
 
   cartQuantity = 0;
+  user!: User;
 
-  constructor(cartService:CartService) {
+  constructor(cartService:CartService, private userService:UserService) {
     cartService.getCartObservable().subscribe(newCart=>{
       this.cartQuantity = newCart.totalCount;
+    });
+
+    userService.userObservable.subscribe((newUser)=>{
+      this.user = newUser;
     });
    }
 
@@ -20,6 +27,13 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  logout(){
+    this.userService.logout();
+  }
+
+  get isAuth() {
+    return this.user && this.user.token;
+  }
 
 
 }
